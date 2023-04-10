60abd149e13d555ea2aa9fe5611dd694b62c5655
eriawan <eriawan@live.com>
2023-04-11 04:33:59 +0700

    Add project to define API version constants and finish sample RAW string of getting build definitions.

--- src/AzureDevOpsRESTPlayground/Program.cs
+++ src/AzureDevOpsRESTPlayground/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace AzureDevOpsRESTPlayground;
+﻿using Rx.AzDo.DistributedTask.VariableGroups;
+using System.Net.Http.Json;
+
+namespace AzureDevOpsRESTPlayground;
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, Azure DevOps REST Playground!");
         // TEST caling Azure DevOps REST API using HttpClient here
+        // Test calling REST API to get a list of all build definitions
+        var webClient = new HttpClient();
+        var orgname = "yourorganizationname";
+        var teamprojectName = "teamprojectName";
+        var azdoPAT = "yourPAT";
+        webClient.DefaultRequestHeaders.Add("Authorization", $"Basic {azdoPAT}");
+        var rawBuildDefinitions = await webClient.GetFromJsonAsync<TfsVariableGroups>($"https://dev.azure.com/{orgname}/{teamprojectName}/_apis/build/definitions?api-version=7.0");
+        Console.WriteLine(rawBuildDefinitions);
     }
 }
---
+++ src/Rx.AzDo.RestApiHelpers/TfsApiVersionConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rx.AzDo.RestApiHelpers
+{
+    public static class TfsApiVersionConstants
+    {
+        public const string VERSION_5_0 = "5.0";
+        public const string VERSION_5_1 = "5.1";
+        public const string VERSION_6_0 = "6.0";
+        public const string VERSION_7_0 = "7.0";
+    }
+}
